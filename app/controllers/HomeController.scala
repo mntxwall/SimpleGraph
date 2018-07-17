b0481e5bebb00db5b650c35614c9ccf085837912
cw <mntcw@aliyun.com>
2018-07-17 23:10:25 +0800

    use assetsfinder replace route.assert

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 /**
@@ -9,7 +8,7 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: AssetsFinder)extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +17,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index() = Action {
     Ok(views.html.index())
   }
 }
